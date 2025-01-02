5a6d87c87f55c795c37cd90aa1d25cbefb604413
Edwin Plasencia Hernández <alu0101329888@ull.edu.es>
2025-01-02 17:27:43 +0000

    Updates

--- assets/src/search.js
+++ assets/src/search.js
@@ -46,6 +46,7 @@ class JekyllSearch {
   
     // https://stackoverflow.com/questions/43431550/async-await-class-constructor
     init() {
+      
       const url = new URL(document.location)
       if (url.searchParams.get("search")) {
         this.searchField.value = url.searchParams.get("search")
